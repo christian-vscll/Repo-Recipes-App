400b40d064ae12fa4d3285ec89fb723645a97858
RobertoHortaFilho <robertohortafilho@gmail.com>
2022-09-23 16:24:41 -0300

    configuraçoes iniciais

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/meals" component={ Login } />
+          <Route exact path="/drinks" component={ Login } />
+          <Route exact path="/meals/:id-da-receita" component={ Login } />
+          <Route exact path="/drinks/:id-da-receita" component={ Login } />
+          <Route exact path="/meals/:id-da-receita/in-progress" component={ Login } />
+          <Route exact path="/drinks/:id-da-receita/in-progress" component={ Login } />
+          <Route exact path="/profile" component={ Login } />
+          <Route exact path="/done-recipes" component={ Login } />
+          <Route exact path="/favorite-recipes" component={ Login } />
+        </Switch>
+      </BrowserRouter>
+      {/* <span className="logo">TRYBE</span>
       <object
         className="rocksGlass"
         type="image/svg+xml"
         data={ rockGlass }
       >
         Glass
-      </object>
+      </object> */}
     </div>
   );
 }
