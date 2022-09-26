27e0e843d8edffc943fe616837eddebd43023dae
Christian Vasconcellos <christian.vscll@gmail.com>
2022-09-26 14:35:09 -0300

    Req 16 a 18 corrigidos e passando

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Router, useHistory } from 'react-router-dom';
+// import { Router } from 'react-router-dom';
+// import { createMemoryHistory } from 'history';
 import Recipes from '../pages/Recipes';
+import renderWithRouter from './renderWithRouter';
+// import App from '../App';
 
-// test('Testes footer 1', () => {
-//   renderWithRouter(<Recipes />);
-//   const drinkIcon = screen.getByTestId('drinks-bottom-btn');
-//   const mealIcon = screen.getByTestId('meals-bottom-btn');
-//   expect(drinkIcon).toBeInTheDocument();
-//   expect(mealIcon).toBeInTheDocument();
-// });
+test('Testes footer 1', () => {
+  renderWithRouter(<Recipes />);
+  const drinkIcon = screen.getByTestId('drinks-bottom-btn');
+  const mealIcon = screen.getByTestId('meals-bottom-btn');
+  expect(drinkIcon).toBeInTheDocument();
+  expect(mealIcon).toBeInTheDocument();
+});
 
 test('Testes footer 2', () => {
-  render(
-    <Router>
-      <Recipes />
-    </Router>,
-  );
-  const history = useHistory();
+  const { history } = renderWithRouter(<Recipes />);
+  history.push('/meals');
+  // console.log(history.push);
+  // console.log(screen);
   const mealIcon = screen.getByTestId('meals-bottom-btn');
   userEvent.click(mealIcon);
-  // console.log(tela);
-  console.log(history);
-  // expect(history.location.pathname).toMatch('/meals');
+  expect(history.location.pathname).toMatch('/meals');
+
+  const drinkIcon = screen.getByTestId('drinks-bottom-btn');
+  userEvent.click(drinkIcon);
+  expect(history.location.pathname).toMatch('/drinks');
 });
