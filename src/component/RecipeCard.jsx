adf21492f7b4bc42652c5ed60b6c8fffa9977f6d
Bruno <brunocfaraco@hotmail.com.br>
2022-09-27 17:47:54 -0300

    refatoração do código para fazer os fetchs necessários da API

--- src/component/RecipeCard.jsx
+++ src/component/RecipeCard.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { meals, mealsCategories } from '../helpers/mockFoodsData';
-import { drinks, drinkCategories } from '../helpers/mockDrinksData';
 
 function RecipeCard() {
   const history = useHistory();
+  const isMeals = history.location.pathname === '/meals';
 
+  const [originalMeals, setOriginalMeals] = useState([]);
+  const [originalDrinks, setOriginalDrinks] = useState([]);
+  const [originalMealsCategories, setOriginalMealsCategories] = useState([]);
+  const [originalDrinksCategories, setOriginalDrinksCategories] = useState([]);
+
+  const getMeals = async () => {
+    const responseMeals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const dataMeals = await responseMeals.json();
+    const { meals } = dataMeals;
+    setOriginalMeals(meals);
+    const responseDrinks = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const dataDrinks = await responseDrinks.json();
+    const { drinks } = dataDrinks;
+    setOriginalDrinks(drinks);
+  };
+
+  const getMealsCategories = async () => {
+    const responseMealsCategories = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+    const dataMealsCategories = await responseMealsCategories.json();
+    const { meals } = dataMealsCategories;
+    setOriginalMealsCategories(meals);
+    const responseDrinksCategories = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+    const dataDrinksCategories = await responseDrinksCategories.json();
+    const { drinks } = dataDrinksCategories;
+    setOriginalDrinksCategories(drinks);
+  };
+
+  useEffect(() => {
+    getMeals();
+    getMealsCategories();
+  }, []);
   const NUMBER_OF_ITEMS_NEEDED = 12;
   const NUMBER_OF_CATEGORIES_NEEDED = 5;
-  const splitFoodData = meals.slice(0, NUMBER_OF_ITEMS_NEEDED);
-  const splitFoodCategoriesData = mealsCategories.slice(0, NUMBER_OF_CATEGORIES_NEEDED);
+  const splitFoodData = originalMeals.slice(0, NUMBER_OF_ITEMS_NEEDED);
+  const splitFoodCategoriesData = originalMealsCategories
+    .slice(0, NUMBER_OF_CATEGORIES_NEEDED);
   // console.log('Food Data');
   // console.log(splitFoodData);
   // console.log(splitFoodCategoriesData);
 
-  const splitDrinkData = drinks.slice(0, NUMBER_OF_ITEMS_NEEDED);
-  const splitDrinksCategoriesData = drinkCategories.slice(0, NUMBER_OF_CATEGORIES_NEEDED);
+  const splitDrinkData = originalDrinks.slice(0, NUMBER_OF_ITEMS_NEEDED);
+  const splitDrinksCategoriesData = originalDrinksCategories
+    .slice(0, NUMBER_OF_CATEGORIES_NEEDED);
   // console.log('Drink Data');
   // console.log(splitDrinkData);
   // console.log(splitDrinksCategoriesData);
@@ -56,6 +88,16 @@ function RecipeCard() {
     ))
   );
 
+  const handleFoodsCatButtons = (category) => {
+    const categoryFixed = category.split(' ').join('_');
+    console.log(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryFixed}`);
+  };
+
+  const handleDrinksCatButtons = (category) => {
+    const categoryFixed = category.split(' ').join('_');
+    console.log(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoryFixed}`);
+  };
+
   // função que vai criar os botões de categoria dinamicamente
   const categorieButtonCreator = (categories) => (
     categories.map(({ strCategory }, index) => (
@@ -63,13 +105,15 @@ function RecipeCard() {
         data-testid={ `${strCategory}-category-filter` }
         key={ index }
         type="button"
+        className="recipe-card-buttons"
+        onClick={ isMeals
+          ? () => handleFoodsCatButtons(strCategory)
+          : () => handleDrinksCatButtons(strCategory) }
       >
         { strCategory }
       </button>
     ))
   );
-
-  const isMeals = history.location.pathname === '/meals';
 
   return (
     <div className="recipe-card-div">
