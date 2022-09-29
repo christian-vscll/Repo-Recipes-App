348115059f2bc45101e7509c2ce15850ba962e15
Bruno <brunocfaraco@hotmail.com.br>
2022-09-29 14:36:25 -0300

    gthjdasuitas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { saveEmail,
   saveMealsToken, saveDrinksToken } from '../tests/helper/localStorage';
-import Header from '../component/Header';
-import Footer from '../component/Footer';
 
 function Login() {
   const history = useHistory();
