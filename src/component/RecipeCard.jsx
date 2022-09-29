d9034ec9ef051692d4d4157632d6171c78c719c2
Bruno <brunocfaraco@hotmail.com.br>
2022-09-28 21:10:08 -0300

    finalização do requisito 21 e 22

--- src/component/RecipeCard.jsx
+++ src/component/RecipeCard.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
+const NUMBER_OF_ITEMS_NEEDED = 12;
+const NUMBER_OF_CATEGORIES_NEEDED = 5;
+
 function RecipeCard() {
   const history = useHistory();
   const isMeals = history.location.pathname === '/meals';
@@ -15,51 +18,38 @@ function RecipeCard() {
   const [mealCategorySelectedArray, setMealCategorySelectedArray] = useState([]);
   const [drinkCategorySelected, setDrinkCategorySelected] = useState('');
   const [drinkCategorySelectedArray, setDrinkCategorySelectedArray] = useState([]);
+  const [toggleOn, setToggleOn] = useState(false);
 
   const getMeals = async () => {
     const responseMeals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
     const dataMeals = await responseMeals.json();
     const { meals } = dataMeals;
-    setOriginalMeals(meals);
-    setRenderedMeals(meals);
+    const splitFoodData = meals.slice(0, NUMBER_OF_ITEMS_NEEDED);
+    setOriginalMeals(splitFoodData);
+    setRenderedMeals(splitFoodData);
     const responseDrinks = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
     const dataDrinks = await responseDrinks.json();
     const { drinks } = dataDrinks;
-    setOriginalDrinks(drinks);
-    setRenderedDrinks(drinks);
+    const splitDrinkData = drinks.slice(0, NUMBER_OF_ITEMS_NEEDED);
+    setOriginalDrinks(splitDrinkData);
+    setRenderedDrinks(splitDrinkData);
   };
 
   const getMealsCategories = async () => {
     const responseMealsCategories = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
     const dataMealsCategories = await responseMealsCategories.json();
     const { meals } = dataMealsCategories;
-    setOriginalMealsCategories(meals);
+    setOriginalMealsCategories(meals.slice(0, NUMBER_OF_CATEGORIES_NEEDED));
     const responseDrinksCategories = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
     const dataDrinksCategories = await responseDrinksCategories.json();
     const { drinks } = dataDrinksCategories;
-    setOriginalDrinksCategories(drinks);
+    setOriginalDrinksCategories(drinks.slice(0, NUMBER_OF_CATEGORIES_NEEDED));
   };
 
   useEffect(() => {
     getMeals();
     getMealsCategories();
   }, []);
-
-  const NUMBER_OF_ITEMS_NEEDED = 12;
-  const NUMBER_OF_CATEGORIES_NEEDED = 5;
-  const splitFoodData = renderedMeals.slice(0, NUMBER_OF_ITEMS_NEEDED);
-  const splitFoodCategoriesData = originalMealsCategories
-    .slice(0, NUMBER_OF_CATEGORIES_NEEDED);
-  // console.log('Food Data');
-  // console.log(splitFoodData);
-  // console.log(splitFoodCategoriesData);
-
-  const splitDrinkData = renderedDrinks.slice(0, NUMBER_OF_ITEMS_NEEDED);
-  const splitDrinksCategoriesData = originalDrinksCategories
-    .slice(0, NUMBER_OF_CATEGORIES_NEEDED);
-  // console.log('Drink Data');
-  // console.log(splitDrinkData);
-  // console.log(splitDrinksCategoriesData);
 
   const foodCardsCreator = (data) => (
     data.map(({
@@ -97,55 +87,55 @@ function RecipeCard() {
     ))
   );
 
-  // useEffect(() => {
+  const handleAllButton = (string) => {
+    if (string === 'food') setRenderedMeals(originalMeals);
+    else setRenderedDrinks(originalDrinks);
+  };
 
-  // }, []);
-
-  const getMealCategoryArray = async (categoryFixed) => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryFixed}`);
+  const getMealCategoryArray = async (category) => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
     const data = await response.json();
     setMealCategorySelectedArray(data);
+    const { meals } = data;
+    setRenderedMeals(meals.slice(0, NUMBER_OF_ITEMS_NEEDED));
   };
 
   const handleFoodsCatButtons = (category) => {
-    setMealCategorySelected(category);
-
-    const categoryFixed = category.split(' ').join('_');
-    getMealCategoryArray(categoryFixed);
+    if (!toggleOn) {
+      setMealCategorySelected(category);
+      getMealCategoryArray(category);
+    } else {
+      handleAllButton('food');
+    }
+    setToggleOn((prevState) => !prevState);
   };
 
-  // useEffect(() => {
-  //   setRenderedDrinks([]);
-  // }, [drinkCategorySelectedArray]);
-
-  const getDrinkCategoryArray = async (categoryFixed) => {
-    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoryFixed}`);
+  const getDrinkCategoryArray = async (category) => {
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
     const data = await response.json();
     setDrinkCategorySelectedArray(data);
-    // const teste = [
-    //   {
-    //     strDrink: '3-Mile Long Island Iced Tea',
-    //     strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg',
-    //     idDrink: '15300',
-    //   },
-    // ];
-    setRenderedDrinks(data);
-    // console.log(originalDrinks);
+    const { drinks } = data;
+    setRenderedDrinks(drinks.slice(0, NUMBER_OF_ITEMS_NEEDED));
+    console.log(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
   };
 
-  const handleDrinksCatButtons = (category) => {
-    setDrinkCategorySelected(category);
-
-    const categoryFixed = category.split(' ').join('_');
-    getDrinkCategoryArray(categoryFixed);
+  const handleDrinksCatButtons = async (category) => {
+    if (!toggleOn) {
+      setDrinkCategorySelected(category);
+      getDrinkCategoryArray(category);
+    } else {
+      handleAllButton('drinks');
+    }
+    setToggleOn((prevState) => !prevState);
   };
 
   // função que vai criar os botões de categoria dinamicamente
-  const categorieButtonCreator = (categories) => (
+  const categorieButtonCreator = (categories, string) => (
     <div>
       <button
         data-testid="All-category-filter"
         type="button"
+        onClick={ () => handleAllButton(string) }
       >
         All
       </button>
@@ -168,10 +158,10 @@ function RecipeCard() {
   return (
     <div className="recipe-card-div">
       <h1>RECIPE CARDS</h1>
-      { isMeals ? foodCardsCreator(splitFoodData) : drinkCardsCreator(splitDrinkData) }
+      { isMeals ? foodCardsCreator(renderedMeals) : drinkCardsCreator(renderedDrinks) }
       { isMeals
-        ? categorieButtonCreator(splitFoodCategoriesData)
-        : categorieButtonCreator(splitDrinksCategoriesData) }
+        ? categorieButtonCreator(originalMealsCategories, 'food')
+        : categorieButtonCreator(originalDrinksCategories, 'drinks') }
     </div>
   );
 }
