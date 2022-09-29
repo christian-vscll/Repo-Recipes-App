aae29a607143994111dbd2b536cccb1979011b20
Bruno <brunocfaraco@hotmail.com.br>
2022-09-28 21:39:51 -0300

    inicio da criação dos testes

---
+++ src/tests/Recipes.test.js
@@ -0,0 +1,46 @@
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { Router } from 'react-router-dom';
+// import { createMemoryHistory } from 'history';
+// import Recipes from '../pages/Recipes';
+// import renderWithRouter from './renderWithRouter';
+// import App from '../App';
+
+describe('It is going to test Recipes component', () => {
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+
+  test('', () => {
+
+  });
+});
